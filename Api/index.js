40a22c5f4ee861a2728f0578d09c69a34b42a775
Kedar200 <Kedardeshmukh2003@gmail.com>
2023-06-07 16:52:52 +0530

    Cancel Ticket Added

--- Api/index.js
+++ Api/index.js
@@ -69,22 +69,22 @@ app.post("/savedata", function (req, res) {
 app.post("/Booke_ticket", function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST");
-  const data = new transaction_db({
-    user_name: req.body.user_name,
-    phone_no: req.body.phone_no,
-    email: req.body.email,
-    ticket_no: req.body.ticket_no,
-    source: req.body.source,
-    destination: req.body.destination,
-    date: req.body.date,
-    Agency: req.body.Agency,
-    pickup_time: req.body.pickup_time,
-    drop_time: req.body.drop_time,
-    price: req.body.price,
-  });
-  data.save();
+  try {
+    const data = new transaction_db({
+      user_name: req.body.user_name,
+      phone_no: req.body.phone_no,
+      email: req.body.email,
+      ticket_no: req.body.ticket_no,
+      source: req.body.source,
+      destination: req.body.destination,
+      date: req.body.date,
+      Agency: req.body.Agency,
+      pickup_time: req.body.pickup_time,
+      drop_time: req.body.drop_time,
+      price: req.body.price,
+    });
 
-  const body = `
+    const body = `
   <!DOCTYPE html>
 <html lang="en">
 
@@ -168,9 +168,6 @@ app.post("/Booke_ticket", function (req, res) {
     <tr>
     <th>Reservation Up to</th>
     <th>Arrival At</th>
-      <th>Bus Agency</th>
-      <th>Date Of Boarding</th>
-      <th>Scheduled Departure</th>
       
     </tr>
     <tr>
@@ -183,12 +180,45 @@ app.post("/Booke_ticket", function (req, res) {
 Thank you for choosing our services. We wish you a pleasant journey!</p>
   `;
 
-  sendmail(req.body.email, body);
-  res.send("ok");
+    data.save();
+
+    sendmail(req.body.email, body);
+    res.send("ok");
+  } catch (error) {
+    console.log("hello");
+    res.send("notok", 500);
+  }
+});
+
+app.post("/Cancel_Ticket", async function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  console.log(req.body);
+  let ticket = await transaction_db.findOne({
+    ticket_no: req.body.ticket_no,
+    user_name: req.body.user_name,
+    phone_no: req.body.phone_no,
+    email: req.body.email,
+  });
+
+  if (!ticket) {
+    res.status(400).send("Wrong details");
+  } else if (ticket.user_name === req.body.user_name) {
+    console.log(ticket);
+    res.send("You have Successfully Submited");
+  } else {
+    res.send("You are not authorized to cancel this ticket");
+  }
+  await transaction_db.deleteOne({
+    ticket_no: req.body.ticket_no,
+    user_name: req.body.user_name,
+    phone_no: req.body.phone_no,
+    email: req.body.email,
+  });
 });
 
 app.post("*", function (req, res) {
-  res.send("You have posted a wrong Request", 404);
+  res.status(404).send("You have posted a wrong Request");
 });
 
 app.listen(80, () => {
--- Public/canceltkt/canceltkt.js
+++ Public/canceltkt/canceltkt.js
@@ -1,30 +1,29 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const source = urlParams.get("mycity1");
-const desti = urlParams.get("mycity2");
+function myFunction() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("user_name", document.querySelector("#name").value);
+    urlencoded.append("phone_no", document.querySelector("#Phone_no").value);
+    urlencoded.append("email", document.querySelector("#email").value);
+    urlencoded.append("ticket_no", document.querySelector("#tktid").value);
 
-fetch(`http://localhost/search_bus?source=${source}&desti=${desti}`)
-  .then((buses) => {
-    return buses.json();
-  })
-  .then((post) => {
-    console.log(post);
-    post.forEach((element) => {
-      $("#buses").append(`<div id="details" class="mainbox">
-      <div id="box1" class="cbox">Agency Name: <p>${element.Agency}</p> </div>
-      <div id="box2" class="cbox">Source:<p>${element.source}</p>  </div>
-      <div id="box3" class="cbox">Destination:<p>${element.destination}</p>  </div>
-      <div id="box4" class="cbox">Pick-up Time: <p>${element.pickup_time}</p> </div>
-      <div id="box5" class="cbox">Drop Time:<p>${element.drop_time}</p>  </div>
-      <div id="box6" class="cbox">Price: <p>${element.price}</p> </div>
-      <div class="bookbtn" onclick=myFunction("${element.source}","${element.destination}")>Book Now</div>
-  </div>`);
-    });
-    if (post.length === 0) {
-      $("#buses").append(`<p class="error">No Buses Found</p>`);
-    }
-  });
-
-function myFunction(a, b) {
-  alert(`\nCongratulations!!\nYour ${a} to ${b} Ticket is Booked`);
+    var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: "follow",
+    };
+    fetch("httP://localhost/Cancel_Ticket", requestOptions)
+        .then((response) => response)
+        .then((result) => {
+            console.log(result.status);
+            if (result.status === 500) {
+                alert("Server Error Ocurred");
+            } else if (result.status === 400) {
+                alert("wrong details");
+            } else {
+                alert("Ticket Canceled");
+            }
+        })
+        .catch((error) => alert("Error Ocurred"));
 }
